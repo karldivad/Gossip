93a06d621d182b19cecb8be7d24e689ff5457f9a
karldivad <carlosvalenciarivero@gmail.com>
2019-04-02 11:51:40 -0500

    Introduce Post show controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'home/index'
 
   resources :users, only: [:show, :edit, :update]
-  resources :posts, only: [:new, :create]
+  resources :posts, only: [:show, :new, :create]
   
   root to: 'home#index'
 end
