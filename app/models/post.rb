d303ee305c2b2ba9c583e9beefcc8071a1a2e95d
karldivad <carlosvalenciarivero@gmail.com>
2019-04-02 12:27:58 -0500

    Add HashTag models

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,9 @@ class Post < ApplicationRecord
   has_one_attached :image
   validate :image_presence
 
+  has_many :post_hash_tags
+  has_many :hash_tags, through: :post_hash_tags
+
   def image_presence
     errors.add(:image, "can't be blank") unless image.attached?
   end
