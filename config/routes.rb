0a6d3cf0af6de73d809426f101165149a9e29772
karldivad <carlosvalenciarivero@gmail.com>
2019-04-01 21:33:49 -0500

    Add edit route and base structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
-  resources :users, only: [:show]
+
+  resources :users, only: [:show, :edit]
   
   root to: 'home#index'
 end
