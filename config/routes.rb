eeed90f8eecebaed808970ecb06b24a31affd39d
karldivad <carlosvalenciarivero@gmail.com>
2019-04-01 22:26:24 -0500

    Add posts basic styles, controllers and db

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
 
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
+  resources :posts, only: [:new, :create]
   
   root to: 'home#index'
 end
