0ff547768a6c0758654e0a94c0f7d8e83220568d
karldivad <carlosvalenciarivero@gmail.com>
2019-04-07 11:56:36 -0500

    Add images validators and amazon S3

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,13 +3,22 @@ class Post < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
-  validate :image_presence
+  validate :image_validate
 
   has_many :post_hash_tags
   has_many :hash_tags, through: :post_hash_tags
 
-  def image_presence
+  def image_validate
     errors.add(:image, "can't be blank") unless image.attached?
+    if image.attached?
+      if image.blob.byte_size > 5000000
+        image.purge
+        errors.add(:image, "should be less than 5MB")
+      elsif !image.blob.content_type.starts_with?('image/')
+        image.purge
+        errors.add(:image, "Wrong format")
+      end
+    end
   end
 
   def create_hash_tags
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,18 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
 
   has_one_attached :avatar
+  validate :avatar_validate
+
+   def avatar_validate
+    if avatar.attached?
+      if avatar.blob.byte_size > 2000000
+        avatar.purge
+        errors.add(:avatar, "should be less than 2MB")
+      elsif !avatar.blob.content_type.starts_with?('image/')
+        avatar.purge
+        errors.add(:avatar, "Wrong format")
+      end
+    end
+  end
+
 end
